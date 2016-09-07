32178306359093db1a03554294ba03ba16e44493
Joseph An <an.joseph891@gmail.com>
2016-09-07 01:09:47 -0400

    add extra test case

--- organize_events_test.rb
+++ organize_events_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require_relative 'organize_events'
 
 class TestOrganizeEvents < Minitest::Test
-  def test_simple_events
+  def test_two_event_dates
     events = [
       {date: '2014-01-01', a: 5, b:1},
       {date: '2014-01-01', xyz: 11},
@@ -13,6 +13,21 @@ class TestOrganizeEvents < Minitest::Test
     organized_events = [
       {date: '2014-01-01', a: 5, b:1, xyz: 11 },
       {date: '2014-10-10', qbz: 5, v: 4, q: 1, strpm: -99},
+    ]
+
+    assert_equal organized_events, OrganizeEvents.new(events).organized
+  end
+
+  def test_one_event_date
+    events = [
+      {date: '2014-01-01', a: 5, b:1},
+      {date: '2014-01-01', xyz: 11},
+      {date: '2014-01-01', a: 5, b:1},
+      {date: '2014-01-01', xyz: 11},
+    ]
+
+    organized_events = [
+      {date: '2014-01-01', a: 5, b:1, xyz: 11 },
     ]
 
     assert_equal organized_events, OrganizeEvents.new(events).organized
