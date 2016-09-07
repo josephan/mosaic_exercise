0d7af083ffd2841f35500486b191bbdb189172f8
Joseph An <an.joseph891@gmail.com>
2016-09-06 22:46:25 -0400

    Initialize repo with script and test

---
+++ organize_events.rb
@@ -0,0 +1,13 @@
+class OrganizeEvents
+  attr_reader :organized
+
+  def initilize(events)
+    @organized = organize_events(events)
+  end
+
+  private
+
+  def organize_events(events)
+
+  end
+end
---
+++ organize_events_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require_relative 'organize_events'
+
+class TestOrganizeEvents < Minitest::Test
+  def test_simple_events
+    events = [
+      {date: '2014-01-01', a: 5, b:1},
+      {date: '2014-01-01', xyz: 11},
+      {date: '2014-10-10', qbz: 5},
+      {date: '2014-10-10', v: 4, q: 1, strpm: -99}
+    ]
+
+    organized_events = [
+      {date: 2014-01-01, a: 5, b:1, xyz: 11 },
+      {date: 2014-10-10, qbz: 5, v: 4, q: 1, strpm: -99},
+    ]
+
+    assert_equal organized_events, OrganizeEvents.new(events).organized
+  end
+end
