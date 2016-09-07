511ff640d7fbb377e6ea0ce11aa43832d1997fad
Joseph An <an.joseph891@gmail.com>
2016-09-06 23:27:38 -0400

    Pass first test case

--- organize_events.rb
+++ organize_events.rb
@@ -1,13 +1,15 @@
 class OrganizeEvents
   attr_reader :organized
 
-  def initilize(events)
-    @organized = organize_events(events)
+  def initialize(events)
+    @events = events
+    @organized = organize_events
   end
 
   private
 
-  def organize_events(events)
-
+  def organize_events
+    @events.group_by { |event| event[:date] }
+           .values.map { |array| array.reduce({}, :merge) }
   end
 end
--- organize_events_test.rb
+++ organize_events_test.rb
@@ -11,8 +11,8 @@ class TestOrganizeEvents < Minitest::Test
     ]
 
     organized_events = [
-      {date: 2014-01-01, a: 5, b:1, xyz: 11 },
-      {date: 2014-10-10, qbz: 5, v: 4, q: 1, strpm: -99},
+      {date: '2014-01-01', a: 5, b:1, xyz: 11 },
+      {date: '2014-10-10', qbz: 5, v: 4, q: 1, strpm: -99},
     ]
 
     assert_equal organized_events, OrganizeEvents.new(events).organized
